cb4beb4b7824e26d4f9b8757ea12d72bf73685f9
ethanqm <q.ethan.moore@gmail.com>
2023-10-20 01:48:12 +1100

    working lmao

--- violate.js
+++ violate.js
@@ -1,20 +1,39 @@
+
+
+let window_url = window.location.href
+let video_code = /watch\?v=(.*)&?/.exec(window_url)[1]
+
 //stock embed at 100% size, with current url
-let embed = `<iframe width=100% height=100% src="${window.location.href}" 
-title="YouTube video player" 
-frameborder="0" 
-allow="accelerometer; 
-autoplay; 
-clipboard-write; 
-encrypted-media; 
-gyroscope; 
-picture-in-picture; 
-web-share" 
-allowfullscreen>
-</iframe>`
+let v_iframe = document.createElement('iframe')
+v_iframe.width = '100%'
+v_iframe.height = '100%'
+v_iframe.src = 'https://www.youtube.com/embed/'+video_code
+v_iframe.title = 'Youtube video player'
+v_iframe.frameborder='0'
+v_iframe.allowfullscreen = ''
 
 
-//embed video over movie_player
-document.querySelector("[id=movie_player]").replaceWith(embed):
+function waitForElementToExist(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-//remove error-screen
-document.querySelector("id=error-screen").remove():
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      subtree: true,
+      childList: true,
+    });
+  });
+}
+
+
+waitForElementToExist("[id=movie_player]").then(e => e.replaceWith(v_iframe))
+waitForElementToExist("[id=error-screen]").then(e => e.remove())
+
