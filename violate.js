4d17fc910d688c369027cc61cbd3e3653d8017ae
ethanqm <q.ethan.moore@gmail.com>
2023-12-11 14:02:22 +1100

    stopped embedding, just deleting the error now

--- violate.js
+++ violate.js
@@ -1,16 +1,16 @@
-let window_url = window.location.href
-let video_code = /watch\?v=([a-zA-Z0-9\_-]*)/.exec(window_url)[1]
-let tss = /\&t=([0-9]*)s/.exec(window_url)
+//let window_url = window.location.href
+//let video_code = /watch\?v=([a-zA-Z0-9\_-]*)/.exec(window_url)[1]
+//let tss = /\&t=([0-9]*)s/.exec(window_url)
 
 //stock embed at 100% size, with current url
-let v_iframe = document.createElement('iframe')
-v_iframe.width = '100%'
-v_iframe.height = '800'
-v_iframe.src = 'https://www.youtube.com/embed/'+video_code
-if (tss !== null && tss.length > 1) {v_iframe.src += "&amp; start="+tss[1]}
-v_iframe.title = 'Youtube video player'
-v_iframe.frameborder='0'
-v_iframe.allowfullscreen = ''
+//let v_iframe = document.createElement('iframe')
+//v_iframe.width = '100%'
+//v_iframe.height = '800'
+//v_iframe.src = 'https://www.youtube.com/embed/'+video_code
+//if (tss !== null && tss.length > 1) {v_iframe.src += "&amp; start="+tss[1]}
+//v_iframe.title = 'Youtube video player'
+//v_iframe.frameborder='0'
+//v_iframe.allowfullscreen = ''
 
 
 //
@@ -40,4 +40,4 @@ function waitForElementToExist(selector) {
 }
 
 waitForElementToExist("[id=error-screen]").then(e => {console.log("removing error-screen");e.remove()})
-waitForElementToExist("[id=player-container-outer]").then(e => {console.log("replacing player-container-outer");e.replaceWith(v_iframe)})
+//waitForElementToExist("[id=player-container-outer]").then(e => {console.log("replacing player-container-outer");e.replaceWith(v_iframe)})
